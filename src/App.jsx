8ec8f887af5731e91377d79a4a61346d03e38fc4
Mayankkvv <mayankkumar2325@gmail.com>
2026-08-04 23:41:07 +0530

    Add animated page transitions between routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,54 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import ProjectDetails from './pages/ProjectDetails'
 import './App.css'
 
+const pageVariants = {
+  initial: { opacity: 0, y: 12 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -12 },
+}
+
 function App() {
+  const location = useLocation()
+
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects/:projectId" element={<ProjectDetails />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <motion.div
+                variants={pageVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ duration: 0.35, ease: 'easeInOut' }}
+              >
+                <Home />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/projects/:projectId"
+            element={
+              <motion.div
+                variants={pageVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ duration: 0.35, ease: 'easeInOut' }}
+              >
+                <ProjectDetails />
+              </motion.div>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </>
   )
 }
