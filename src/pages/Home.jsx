3a8569e26e3afd046bf054a24c5d2cc74e5e920b
Mayankkvv <mayankkumar2325@gmail.com>
2026-08-04 13:22:02 +0530

    Set up React Router with Home and ProjectDetails pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>This will become the hero, about, skills, and contact sections.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/ProjectDetails.jsx
@@ -0,0 +1,10 @@
+function ProjectDetails() {
+  return (
+    <div>
+      <h1>Project Details Page</h1>
+      <p>This will show one project's full details (image, tech stack, architecture, links).</p>
+    </div>
+  );
+}
+
+export default ProjectDetails;
